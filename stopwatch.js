898e242dec8f4f9bfcad6835debc2caa5de8f6ef
Vinod Kumar Gupta <vkgupta857@gmail.com>
2021-08-04 23:20:42 +0530

    add hide/unhide button

--- stopwatch.js
+++ stopwatch.js
@@ -8,6 +8,36 @@ var darkBackgroundColor = "black";
 var lightTextColor = "black";
 var lightBackgroundColor = "white";
 
+//variables
+var stopwatchTimer = 0; //time counter
+var action; //variable for setInterval
+
+//minutes,seconds,centiseconds for time and lap
+var timeMinutes, timeSeconds, timeCentiseconds;
+
+var lapCount = 0;
+var lapTime = 0;
+var stopwatchTimerDisplay = document.getElementById("stopwatchTimerDisplay");
+var startStopwatchBtn = document.getElementById("startStopwatchBtn");
+var stopStopwatchBtn = document.getElementById("stopStopwatchBtn");
+var resetStopwatchBtn = document.getElementById("resetStopwatchBtn");
+var lapStopwatchBtn = document.getElementById("lapStopwatchBtn");
+var resumeStopwatchBtn = document.getElementById("resumeStopwatchBtn");
+
+var startTimerBtn = document.getElementById("startTimerBtn");
+var stopTimerBtn = document.getElementById("stopTimerBtn");
+var resetTimerBtn = document.getElementById("resetTimerBtn");
+
+function initialSetup() {
+  stopStopwatchBtn.style.display = "none";
+  resumeStopwatchBtn.style.display = "none";
+  lapStopwatchBtn.style.display = "none";
+  resetStopwatchBtn.style.display = "none";
+
+  stopTimerBtn.style.display = "none";
+  resetTimerBtn.style.display = "none";
+}
+
 function toggleLightTheme(checkbox) {
   if (checkbox.checked) {
     // console.log("light");
@@ -20,63 +50,74 @@ function toggleLightTheme(checkbox) {
   }
 }
 
-function openFullscreen() {
-  var elem = document.getElementById("timer");
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) {
-    /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) {
-    /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) {
-    /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
-//variables
-var timeCounter = 0; //time counter
-var action; //variable for setInterval
-
-//minutes,seconds,centiseconds for time and lap
-var timeMinutes,
-  timeSeconds,
-  timeCentiseconds;
-
-var stopwatchTimerDisplay = document.getElementById("stopwatchTimerDisplay");
-
 function startStopwatch() {
   action = setInterval(function () {
-    timeCounter++;
-    if (timeCounter == 100 * 60 * 100) {
-      timeCounter = 0;
-      console.log(timeCounter);
+    stopwatchTimer++;
+    if (stopwatchTimer == 100 * 60 * 100) {
+      stopwatchTimer = 0;
+      console.log(stopwatchTimer);
     }
-    
     updateTime();
   }, 10);
+  startStopwatchBtn.style.display = "none";
+  stopStopwatchBtn.style.display = "inline-block";
+  lapStopwatchBtn.style.display = "inline-block";
+  resetStopwatchBtn.style.display = "inline-block";
 }
 
 function stopStopwatch() {
   clearInterval(action);
+  resumeStopwatchBtn.style.display = "inline-block";
+  lapStopwatchBtn.style.display = "none";
+  resetStopwatchBtn.style.display = "inline-block";
+}
+
+function resumeStopwatch() {
+  action = setInterval(function () {
+    stopwatchTimer++;
+    if (stopwatchTimer == 100 * 60 * 100) {
+      stopwatchTimer = 0;
+      console.log(stopwatchTimer);
+    }
+    updateTime();
+  }, 10);
+  resumeStopwatchBtn.style.display = "none";
+  lapStopwatchBtn.style.display = "inline-block";
 }
 
 function resetStopwatch() {
-    stopwatchTimerDisplay.innerHTML = "00:00:00";
+  clearInterval(action);
+  stopwatchTimer = 0;
+  lapCount = 0;
+  stopwatchTimerDisplay.innerHTML = "00:00:00";
+  startStopwatchBtn.style.display = "inline-block";
+  stopStopwatchBtn.style.display = "none";
+  lapStopwatchBtn.style.display = "none";
+  resetStopwatchBtn.style.display = "none";
+  resumeStopwatchBtn.style.display = "none";
 }
 
 //updateTime: converts counters to min,sec,centisec
 function updateTime() {
   //1min=60*100centiseconds=6000centiseconds
-  timeMinutes = Math.floor(timeCounter / 6000);
+  timeMinutes = Math.floor(stopwatchTimer / 6000);
   //1sec=100centiseconds
-  timeSeconds = Math.floor((timeCounter % 6000) / 100);
-  timeCentiseconds = (timeCounter % 6000) % 100;
+  timeSeconds = Math.floor((stopwatchTimer % 6000) / 100);
+  timeCentiseconds = (stopwatchTimer % 6000) % 100;
+  timeToDisplay =
+    format(timeMinutes) +
+    ":" +
+    format(timeSeconds) +
+    ":" +
+    format(timeCentiseconds);
+  lapTime = timeToDisplay;
+  stopwatchTimerDisplay.innerHTML = timeToDisplay;
+}
 
-  stopwatchTimerDisplay.innerHTML =
-    format(timeMinutes) + ":" + format(timeSeconds) + ":" + format(timeCentiseconds);
+function lap() {
+  console.log("lap ", lapCount,lapTime);
+  // TODO: add code to insert row in table
+  lapCount++;
 }
 
 //format numbers
