ce760869dda221b4db590350c764362a79cabb4f
Vinod Kumar Gupta <vkgupta857@gmail.com>
2021-08-04 22:37:27 +0530

    updated clock style and theme

--- stopwatch.js
+++ stopwatch.js
@@ -1,128 +1,89 @@
 //stopwatch.js
-$(function(){
-    //variables
-    var mode = 0;//App mode
-    var timeCounter = 0;//time counter
-    var lapCounter = 0;//lap counter
-    var action;//variable for setInterval
-    var lapNumber = 0;//Number of Laps
-        
-        //minutes,seconds,centiseconds for time and lap
-    var timeMinutes, timeSeconds, timeCentiseconds, lapMinutes, lapSeconds, lapCentiseconds;
-    
-    //On App load show start and lap buttons
-    hideshowButtons("#startButton","#lapButton");
-    //click on startButton
-    $("#startButton").click(function(){
-        //mode on
-        mode = 1;
-        //show stop and lap buttons
-        hideshowButtons("#stopButton","#lapButton");
-        //start counter
-        startAction();
-    });
 
-    
-    //click on stopButton
-    $("#stopButton").click(function(){
-        //show resume and reset buttons
-        hideshowButtons("#resumeButton","#resetButton");
-        //stop counter
-        clearInterval(action);
-    });
-    
-    //click on resumeButton
-    $("#resumeButton").click(function(){
-        //show stop and lap buttons
-        hideshowButtons("#stopButton","#lapButton");
-        //start counter
-        startAction();
-    });
-    
-    //click on resetButton
-    $("#resetButton").click(function(){
-        //reload the page
-        location.reload();
-    });
-    
-    //click on lapButton
-    $("#lapButton").click(function(){
-        //if mode is ON
-        if(mode){
-            //stop action
-            clearInterval(action);
-            //resetLap and print lap details
-            lapCounter = 0;
-            addLap();
-            //start action
-            startAction();
-        }
-    });
+//Dark theme vars
+var darkTextColor = "white";
+var darkBackgroundColor = "black";
 
-    
-    //functions
-    //hideshowButtons function shows two buttons
-    function hideshowButtons(x,y){
-        $(".control").hide();
-        $(x).show();
-        $(y).show();
+//Light theme vars
+var lightTextColor = "black";
+var lightBackgroundColor = "white";
+
+function toggleLightTheme(checkbox) {
+  if (checkbox.checked) {
+    // console.log("light");
+    document.body.style.backgroundColor = lightBackgroundColor;
+    document.body.style.color = lightTextColor;
+  } else {
+    // console.log("dark");
+    document.body.style.backgroundColor = darkBackgroundColor;
+    document.body.style.color = darkTextColor;
+  }
+}
+
+function openFullscreen() {
+  var elem = document.getElementById("timer");
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) {
+    /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+//variables
+var timeCounter = 0; //time counter
+var action; //variable for setInterval
+
+//minutes,seconds,centiseconds for time and lap
+var timeMinutes,
+  timeSeconds,
+  timeCentiseconds;
+
+var stopwatchTimerDisplay = document.getElementById("stopwatchTimerDisplay");
+
+function startStopwatch() {
+  action = setInterval(function () {
+    timeCounter++;
+    if (timeCounter == 100 * 60 * 100) {
+      timeCounter = 0;
+      console.log(timeCounter);
     }
     
-    //start the counter
-    function startAction(){
-        action = setInterval(function(){
-            timeCounter++;
-            if(timeCounter == 100*60*100){
-                timeCounter = 0;   
-            }
-            lapCounter++;
-            if(lapCounter == 100*60*100){
-                lapCounter = 0;   
-            }
-            updateTime();
-        },10);
-    }
-    
-    //updateTime: converts counters to min,sec,centisec
-    function updateTime(){
-        //1min=60*100centiseconds=6000centiseconds
-        timeMinutes = Math.floor(timeCounter/6000);
-        //1sec=100centiseconds
-        timeSeconds = Math.floor((timeCounter%6000)/100);
-        timeCentiseconds = (timeCounter%6000)%100;
-        $("#timeminute").text(format(timeMinutes));
-        $("#timesecond").text(format(timeSeconds));
-        $("#timecentisecond").text(format(timeCentiseconds));
-        
-        //1min=60*100centiseconds=6000centiseconds
-        lapMinutes = Math.floor(lapCounter/6000);
-        //1sec=100centiseconds
-        lapSeconds = Math.floor((lapCounter%6000)/100);
-        lapCentiseconds = (lapCounter%6000)%100;
-        $("#lapminute").text(format(lapMinutes));
-        $("#lapsecond").text(format(lapSeconds));
-        $("#lapcentisecond").text(format(lapCentiseconds));
-    }
-    
-    //format numbers
-    function format(number){
-        if(number<10){
-            return '0'+number;   
-        }else{
-            return number;   
-        }
-    }
-    
-    //addLap function: print lap details inside the lap box
-    function addLap(){
-        lapNumber++;
-        var table = document.getElementById("lapTable");
-        var row = table.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        cell1.innerHTML = "# " + lapNumber;
-        cell2.innerHTML = format(lapMinutes) + ':' + format(lapSeconds) + ':' + format(lapCentiseconds);
-        cell3.innerHTML = format(timeMinutes) + ':' + format(timeSeconds) + ':' + format(timeCentiseconds);
-    }
-});
+    updateTime();
+  }, 10);
+}
+
+function stopStopwatch() {
+  clearInterval(action);
+}
+
+function resetStopwatch() {
+    stopwatchTimerDisplay.innerHTML = "00:00:00";
+}
+
+//updateTime: converts counters to min,sec,centisec
+function updateTime() {
+  //1min=60*100centiseconds=6000centiseconds
+  timeMinutes = Math.floor(timeCounter / 6000);
+  //1sec=100centiseconds
+  timeSeconds = Math.floor((timeCounter % 6000) / 100);
+  timeCentiseconds = (timeCounter % 6000) % 100;
+
+  stopwatchTimerDisplay.innerHTML =
+    format(timeMinutes) + ":" + format(timeSeconds) + ":" + format(timeCentiseconds);
+}
+
+//format numbers
+function format(number) {
+  if (number < 10) {
+    return "0" + number;
+  } else {
+    return number;
+  }
+}
